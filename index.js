75ba086d78a7cc4ef9f413fb7c59cf0833beca74
Brigham <brig.johnson@gmail.com>
2022-05-03 01:44:22 -0400

    commit tree f3c890230e9490788c40bb73f5177907dcb21466

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+/*
+We have an array of drivers with various information. We need to write functions using the filter() method so that PickMeUp Taxi service employees can easily query the data.
+*/
+
+function findMatching(names, searchString) {
+// findMatching- This function takes an array of drivers' names and a string as arguments, and returns the matching list of drivers. The function should be case insensitive.
+    return names.filter(name => searchString.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(names, matchString) {
+// fuzzyMatch - This function takes an array of drivers' names and a string as arguments for querying the array, and returns all drivers whose names begin with the provided letters.
+    const matchMe = matchString.toLowerCase();
+    return names.filter(name =>
+        name.toLowerCase().substring(0, matchMe.length) === matchMe);
+}
+
+function matchName(nameObjs, matchString) {
+// matchName - This function takes an array of driver objects and a string as arguments. Each driver object has two properties: name and hometown. The function should return each element whose name property matches the provided string argument.
+    return nameObjs.filter(nameObj => nameObj.name === matchString);
+        //name.toLowerCase().substring(0, matchMe.length) === matchMe);
+
+}
